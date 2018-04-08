2a0d78231eff51c6df640bc136d8a724430bb2e6
glongh <glongh@gmail.com>
2018-04-08 17:32:33 -0400

    Move contacts to component state

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 import ListConstacts from './ListContacts';
 
-const contacts = [
-  {
-    id: 'tyler',
-    name: 'Tyler McGinnis',
-    handle: '@tylermcginnis',
-    avatarURL: 'http://localhost:5001/tyler.jpg'
-  },
-  {
-    id: 'karen',
-    name: 'Karen Isgrigg',
-    handle: '@karen_isgrigg',
-    avatarURL: 'http://localhost:5001/karen.jpg'
-  },
-  {
-    id: 'richard',
-    name: 'Richard Kalehoff',
-    handle: '@richardkalehoff',
-    avatarURL: 'http://localhost:5001/richard.jpg'
-  },  
-]
+
 class App extends Component {
+  state = {
+      contacts : [
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+      },  
+    ]
+  }
   render() {
     return (
-      <ListConstacts contacts={contacts} />
+      <ListConstacts contacts={this.state.contacts} />
     )
   }
 }
