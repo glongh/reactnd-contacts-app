2cdf8e85365d25727ee3d22c236ece6227863f0d
glongh <glongh@gmail.com>
2018-04-08 16:53:12 -0400

    ListComponent created and included into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import ListConstacts from './ListContacts';
 
+const contacts = [
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    handle: '@tylermcginnis',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
+  },
+  {
+    id: 'karen',
+    name: 'Karen Isgrigg',
+    handle: '@karen_isgrigg',
+    avatarURL: 'http://localhost:5001/karen.jpg'
+  },
+  {
+    id: 'richard',
+    name: 'Richard Kalehoff',
+    handle: '@richardkalehoff',
+    avatarURL: 'http://localhost:5001/richard.jpg'
+  },  
+]
 class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <ListConstacts contacts={contacts} />
     )
   }
 }
